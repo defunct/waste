2d77827561d86e13fc3ae23ab7413860d0cd0b38
Alan Gutierrez <alan@blogometer.com>
2010-06-03 02:38:53 -0500

    Mix ProjectModule is now an interface.

--- src/mix/java/com/goodworkalan/waste/mix/WasteProject.java
+++ src/mix/java/com/goodworkalan/waste/mix/WasteProject.java
@@ -4,24 +4,28 @@ import com.goodworkalan.mix.ProjectModule;
 import com.goodworkalan.mix.builder.Builder;
 import com.goodworkalan.mix.builder.JavaProject;
 
-public class WasteProject extends ProjectModule {
-    @Override
+/**
+ * Builds the project definition for Waste.
+ *
+ * @author Alan Gutierrez
+ */
+public class WasteProject implements ProjectModule {
+    /**
+     * Build the project definition for Waste.
+     *
+     * @param builder
+     *          The project builder.
+     */
     public void build(Builder builder) {
         builder
             .cookbook(JavaProject.class)
                 .produces("com.github.bigeasy.waste/waste/0.7")
-                .main()
-                    .depends()
-                        .include("org.freemarker/freemarker/2.3.13")
-                        .include("javax.mail/mail/1.4")
-                        .include("log4j/log4j/1.2.13")
-                        .include("org.slf4j/slf4j-log4j12/1.4.2")
-                        .end()
-                    .end()
-                .test()
-                    .depends()
-                        .include("org.testng/testng-jdk15/5.10")
-                        .end()
+                .depends()
+                    .production("org.freemarker/freemarker/2.3.13")
+                    .production("javax.mail/mail/1.4")
+                    .production("log4j/log4j/1.2.13")
+                    .production("org.slf4j/slf4j-log4j12/1.4.2")
+                    .development("org.testng/testng-jdk15/5.10")
                     .end()
                 .end()
             .end();
